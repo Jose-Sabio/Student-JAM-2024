c5b5d7821843c12bc6e9ed6144b9223e96b46443
Jose Antonio Sabio <joseantoniosabiomolina@outlook.es>
2024-04-20 13:03:33 +0200

    Creacion de movimiento por los menus

--- Assets/Script/MenuHandler.cs
+++ Assets/Script/MenuHandler.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class MenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+      // Este método se llamará cuando se presione un botón u ocurra algún evento que desee cambiar de escena.
+    public void OnBotonInicio()
     {
-        
+        SceneManager.LoadScene("EscenaInicio");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnBotonJugar(){
+        SceneManager.LoadScene("EscenaNave");
+    }
+    public void OnBotonCreditos(){
+        SceneManager.LoadScene("EscenaCreditos");
+    }
+      public void OnBotonControles(){
+        SceneManager.LoadScene("EscenaControles");
     }
 }
+
