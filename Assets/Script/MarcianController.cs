99d01ab77b568a6d7d4a4d47cc9a3c5344d40adb
tigre437 <davidserranojuegos@gmail.com>
2024-04-20 16:39:39 +0200

    cambios enemigos
    - Disparan al jugador
    - Cagan
    - Prefabs de la bala, el marciano y el pajaro

---
+++ Assets/Script/MarcianController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class MarcianController : MonoBehaviour
+{
+    public float velocidad;
+    public Vector3 posicionFin;
+    public GameObject balaPrefab; // Prefab de la bala
+    public float tiempoEntreDisparos = 1f; // Tiempo entre cada disparo
+    public Transform direccionDisparo; // Dirección del disparo
+    private float tiempoUltimoDisparo; // Tiempo del último disparo
+
+    private Vector3 posicionInicio;
+    private bool movimientoAFin;
+
+    private int vida;
+    private int daño;
+
+    void Start()
+    {
+        posicionInicio = transform.position;
+        movimientoAFin = true;
+        tiempoUltimoDisparo = Time.time;
+    }
+
+    void Update()
+    {
+        MoverEnemigo();
+        if (Time.time - tiempoUltimoDisparo >= tiempoEntreDisparos)
+        {
+            Disparar();
+            tiempoUltimoDisparo = Time.time;
+        }
+    }
+
+    private void MoverEnemigo()
+    {
+        Vector3 posicionDestino = (movimientoAFin) ? posicionFin : posicionInicio;
+
+        transform.position = Vector3.MoveTowards(transform.position, posicionDestino, velocidad * Time.deltaTime);
+        if (transform.position == posicionFin)
+            movimientoAFin = false;
+        if (transform.position == posicionInicio)
+            movimientoAFin = true;
+    }
+
+    private void Disparar()
+    {
+        // Instancia la bala
+        GameObject bala = Instantiate(balaPrefab, transform.position, Quaternion.identity);
+
+        // Obtiene la dirección del disparo
+        Vector2 direccion = (direccionDisparo.position - transform.position).normalized;
+
+        // Calcula el ángulo de disparo
+        float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+
+        // Aplica rotación al proyectil
+        bala.transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
+
+        // Aplica fuerza a la bala en la dirección del disparo
+        bala.GetComponent<Rigidbody2D>().velocity = direccion * 10f; // Ajusta la velocidad como desees
+
+        // Destruye la bala después de un tiempo
+        Destroy(bala, 2f); // Cambia el valor de tiempo como desees
+    }
+}
